b9fb7f5cf19fdcede57cc932b30e36e52fcf448f
Dinethmi Peiris <thinupeiris04@gmail.com>
2026-08-04 10:45:33 +0530

    initializing the repository

---
+++ src/main/java/com/vhms/vhms/VhmsApplication.java
@@ -0,0 +1,13 @@
+package com.vhms.vhms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vhms/vhms/VhmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhms.vhms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
